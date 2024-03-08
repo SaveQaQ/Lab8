41f212820421833693f965221d8c5501624c87b8
Jimmy <zhijianliu72@gmail.com>
2024-03-07 17:28:11 -0700

    build

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,9 +58,15 @@ public class CustomList extends ArrayAdapter<City> {
      empty * @param city
      */
 
+    /**
+     * this adds a city object to the list
+     *the second phase, you can add the
+     city * @param city
+     */
     public void addCity(City city){
-
+        cities.add(city);
     }
+
 
 
 }
